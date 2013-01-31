6846d0986a9fb349fb0363fde15e5c7a0b9f4e6a
Benoît Legat <benoit.legat@gmail.com>
2013-01-31 10:07:35 +0100

    Update Gemfile

---
+++ app/models/word.rb
@@ -0,0 +1,25 @@
+### BEGIN LICENSE
+# Copyright (C) 2012 Benoît Legat benoit.legat@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+class Word < ActiveRecord::Base
+  attr_accessible :name
+  VALID_EMAIL_REGEX = /\A[\w]+\z/
+  validates :name, presence: true, length: { maximum: 32 },
+    format: { with: VALID_EMAIL_REGEX }
+
+  belongs_to :language
+end
