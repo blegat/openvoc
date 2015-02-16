cbee00dc365b1b2c2b1d85b51e526235b70a6ebe
Benoît Legat <benoit.legat@gmail.com>
2015-02-16 10:08:19 +0100

    Apply rake rails:update

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
-# Load the rails application
+# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
-app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
-load(app_environment_variables) if File.exists?(app_environment_variables)
-
-# Initialize the rails application
+# Initialize the Rails application.
 Openvoc::Application.initialize!
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,10 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Openvoc::Application.config.secret_token = '6d598b3061b2e43101e26532b89e15cfe0e1472c5f5f730b444d3cd4f52a85464ac406b6a14d4c480b5e0c1f84994ee56fa66115d50f0e4d273283e01d76a576'
-Openvoc::Application.config.secret_key_base = 'xx6d598b3061b2e43101e26532b89e15cfe0e1472c5f5f730b444d3cd4f52a85464ac406b6a14d4c480b5e0c1f84994ee56fa66115d50f0e4d273283e01d76a576'
-# TODO In Rails 4 the configuration option in this file has been renamed from secret_token to secret_key_base. We’ll need to specify both options while we’re transitioning from Rails 3 but once we’ve successfully migrated our application we can remove the secret_token option. It’s best to use a different token for our secret_key_base.
-# http://railscasts.com/episodes/415-upgrading-to-rails-4?view=asciicast
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openvoc::Application.config.secret_key_base = 'd0a0c3af1a99c7808d293381cc8f1e196234af1cf7eb378c7e158cd0394c617f20dda2fe22d938d0a24d9da612a5aba9fdb5c8aaa17e96fa3af3626e1b04c2b6'
