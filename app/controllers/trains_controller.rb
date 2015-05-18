77c442798eb98d58efcffe1589bb31bef15c1cc9
pverbist95 <philippe.verbist@student.uclouvain.be>
2015-05-18 14:28:52 +0200

    Add feedback to trains
    NB: to use this functionality, it is recommended to create a new list,
    with new words

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -2,45 +2,21 @@ class TrainsController < ApplicationController
   before_filter :signed_in_user
   before_filter :list_exists, only: [:create] 
   before_filter :set_rec,     only: [:create] 
-  before_filter :set_max,     only: [:create] 
+  before_filter :set_max,     only: [:create]
+   
   def new    
-    if false
-    words = ((@rec) ? @list.words_rec : @list.words)
-    wordsets = get_wordsets(@list)
-    words = [Word.find_by(239)]
-    if @max < 100
-      words = words.select do |word|
-        word.success_count * 100 <= word.train_count * @max
-      end
-    end
-    if words.empty?
-      redirect_to @list,
-        flash: { info: "There is no word for training" } and return
-    end
-    not_trained_yet = words.select do |w|
-      not w.trained_by(current_user)
-    end
-    if not not_trained_yet.empty?
-      @word = not_trained_yet.at(Random.rand(not_trained_yet.size))
-    else
-      require 'rubygems'
-      require 'algorithms'
-      q = Containers::PriorityQueue.new
-      pool = words
-      message = ""
-      pool.each do |x|
-        message += x.last_train(current_user).inspect
-        q.push(x, Time.now - x.last_train(current_user).updated_at)
-      end
-      @word = q.next
-    end
-  end
   end
   
   def create
     @new_train = current_user.trains.build(list_id: @list.id, finished: false, success_ratio: 0.0, max: @max, actual_ws_id: -1)
     
     initialize_ws(@new_train, @list)
+    
+    if @new_train.word_sets_ids.nil?
+      puts 'NoWordForTraining'
+      redirect_to @list, flash: { info: "There is no word for training" } and return
+    end
+    
     if @new_train.save
       redirect_to @new_train, 
          flash: { success: "Train successfully created" } and return
@@ -53,8 +29,29 @@ class TrainsController < ApplicationController
   
   def show
     @train = Train.find(params[:id]) 
+    @guessTrue = false
+    @guessFalse = false
     
-    if not @train.actual_ws_id == -1
+    if params[:modif_guess]
+      @previous_ws = WordSet.find(params[:modif_guess][:ws_id])
+      @previous_word1 = Word.find(@previous_ws.word1_id) 
+      @previous_word2 = Word.find(@previous_ws.word2_id)
+      if params[:modif_guess][:to] == "true"
+        @guessTrue = true
+        @previous_ws.success += 1
+      else
+        @guessFalse = true
+        @previous_ws.success -= 1
+      end
+      @previous_ws.success_ratio = @previous_ws.success*100 / @previous_ws.asked
+      if not @previous_ws.save
+        redirect_to root_path and return
+      end
+    end
+    
+    else if not @train.actual_ws_id == -1
+      puts 'TRALALA'
+      @previous_ws_id = @train.actual_ws_id
       if not params[:translation].nil?
         check_answer
         pick_new_ws
@@ -80,6 +77,7 @@ class TrainsController < ApplicationController
     @word2 = Word.find(actual_ws.word2_id)
     
   end
+
   
   
 
@@ -88,8 +86,8 @@ class TrainsController < ApplicationController
   def initialize_ws(train, list)
     wordsets = get_wordsets(list)
     if wordsets.empty?
-      redirect_to @list,
-      flash: { info: "There is no word for training" } and return
+      train.word_sets_ids = nil
+      return
     end
     ws_ids = []
     wordsets.each do |ws|
@@ -98,8 +96,8 @@ class TrainsController < ApplicationController
       end
     end
     if ws_ids.empty?
-      redirect_to @list,
-        flash: { info: "There is no word for training" } and return
+      train.word_sets_ids = nil
+      return
     end
     train.word_sets_ids = ws_ids.join(',')
   end
@@ -125,14 +123,18 @@ class TrainsController < ApplicationController
   end
   
   def check_answer
-    ws = WordSet.find(@train.actual_ws_id)
-    word2 = Word.find(ws.word2_id) 
-    if word2.content == params[:translation]
-      ws.success += 1
+    @previous_ws = WordSet.find(@train.actual_ws_id)
+    @previous_word1 = Word.find(@previous_ws.word1_id) 
+    @previous_word2 = Word.find(@previous_ws.word2_id)
+    if @previous_word2.content == params[:translation]
+      @guessTrue = true
+      @previous_ws.success += 1
+    else
+      @guessFalse = true
     end
-    ws.asked += 1
-    ws.success_ratio = ws.success*100 / ws.asked
-    if not ws.save
+    @previous_ws.asked += 1
+    @previous_ws.success_ratio = @previous_ws.success*100 / @previous_ws.asked
+    if not @previous_ws.save
       redirect_to root_path and return
     end
   end
@@ -148,9 +150,11 @@ class TrainsController < ApplicationController
       redirect_to root_path and return
     end
   end
+  
   def set_rec
     @rec = (params[:train][:rec] == "1" ? 1 : 0)
   end
+  
   def list_exists
     @list = List.find_by_id(params[:train][:list_id])
     if @list.nil?
@@ -165,4 +169,6 @@ class TrainsController < ApplicationController
       list.wordsets#.paginate(page: params[:page])
     end
   end
+  
 end
+
